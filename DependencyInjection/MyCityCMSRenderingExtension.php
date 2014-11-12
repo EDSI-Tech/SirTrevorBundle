1a803e3a8e4e3d9f02e761a4a3a03a4c3e709c9c
Romaric Drigon <romaric.drigon@gmail.com>
2014-11-12 17:17:23 +0100

    Bootstrap bundle

---
+++ DependencyInjection/MyCityCMSRenderingExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace EdsiTech\SirTrevorBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
+
+class MyCityCMSRenderingExtension extends Extension
+{
+    /**
+     * @inheritdoc
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+    }
+}
---
+++ EdsiTechSirTrevorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EdsiTech\SirTrevorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EdsiTechSirTrevorBundle extends Bundle
+{
+} 
