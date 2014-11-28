1206078d4ae9b40b47365695345cb09ab7282dc6
Romaric Drigon <romaric.drigon@gmail.com>
2014-11-28 11:28:42 +0100

    Added and using block_render Twig function #2

--- Twig/SirTrevorExtension.php
+++ Twig/SirTrevorExtension.php
@@ -3,6 +3,7 @@
 namespace EdsiTech\SirTrevorBundle\Twig;
 
 use EdsiTech\SirTrevorBundle\Serializer\BlockSerializer;
+use EdsiTech\SirTrevorBundle\Entity\AbstractBlock;
 
 class SirTrevorExtension extends \Twig_Extension
 {
@@ -11,15 +12,21 @@ class SirTrevorExtension extends \Twig_Extension
      */
     private $blockSerializer;
 
+    /**
+     * @var string
+     */
+    private $blocksTheme;
+
 
     const BLOCK_RENDER_TEMPLATE = 'EdsiTechSirTrevorBundle:Render:base.html.twig';
 
     const BLOCK_EDIT_TEMPLATE = 'EdsiTechSirTrevorBundle:Edit:base.html.twig';
 
 
-    public function __construct(BlockSerializer $blockSerializer)
+    public function __construct(BlockSerializer $blockSerializer, $blocksTheme)
     {
-        $this->blockSerializer = $blockSerializer;
+        $this->blockSerializer  = $blockSerializer;
+        $this->blocksTheme      = $blocksTheme;
     }
 
     /**
@@ -29,6 +36,11 @@ class SirTrevorExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFunction('cms_render', [$this, 'cmsRender'], [
+                'needs_context'     => true,
+                'needs_environment' => true,
+                'is_safe'           => ['html']
+            ]),
+            new \Twig_SimpleFunction('block_render', [$this, 'blockRender'], [
                 'needs_context'     => true,
                 'needs_environment' => true,
                 'is_safe'           => ['html']
@@ -70,6 +82,30 @@ class SirTrevorExtension extends \Twig_Extension
         return $environment->render(self::BLOCK_RENDER_TEMPLATE, [
             'blocks' => $blocks
         ]);
+    }
+
+    /**
+     * Render, display, a Block
+     *
+     * @param \Twig_Environment $environment
+     * @param $context
+     * @param AbstractBlock $block
+     * @return string
+     */
+    public function blockRender(\Twig_Environment $environment, $context, AbstractBlock $block)
+    {
+        // Will take care of caching it properly!
+        /** @var \Twig_Template $blocksThemeTemplate */
+        $blocksThemeTemplate = $environment->loadTemplate($this->blocksTheme);
+
+        // Resolve block name
+        $blockName = 'block_'.$block->getType();
+
+        $vars = array_merge($context, [
+            'block' => $block
+        ]);
+
+        return $blocksThemeTemplate->renderBlock($blockName, $vars);
     }
 
     /**
